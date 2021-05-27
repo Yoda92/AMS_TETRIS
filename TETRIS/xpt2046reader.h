25bd634c494a0ba84f461621f30f7b03afb3439e
Yoda92 <andersfisker1007@outlook.com>
2021-05-27 11:58:11 +0200

    Removed old files

--- TETRIS/xpt2046reader.h
+++
@@ -1,8 +0,0 @@
-
-//Uses the spi driver to read xpt2046
-
-
-void initReader();
-struct ADC_read getRawADCCoordates();
-
-unsigned char simplexRead();
--- TETRIS/xpt2046tetris.h
+++
@@ -1,6 +0,0 @@
-#include "CoordinateMapper.h"
-#include <stdbool.h>
-
-void initXPT2046Tetris();
-PlayerAction readLatestPlayerAction();
-extern bool actionReady;
