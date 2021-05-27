3f7e8f93a5d15824664d12cbfabf902bd35e8436
Kristian Lau Jespersen <201807157@post.au.dk>
2021-05-27 13:31:30 +0200

    debounce working

--- TETRIS/XPT2046/xpt2046tetris.c
+++ TETRIS/XPT2046/xpt2046tetris.c
@@ -6,15 +6,10 @@
 
 volatile struct ADC_read raw;
 bool actionReady = false;
+#define CPU_CLOCK 16000000
+#define TIMER_MAX 65535
 
-ISR(INT4_vect)
-{
-	EIMSK &= 0b11101111; // Disable interrupt 4
-	raw = getRawADCCoordates();
-	EIFR |= 0b00010000; // Set interrupt 4 flag
-	EIMSK |= 0b00010000; // Enable interrupt 4
-    actionReady = true;
-}
+
 
 void initXPT2046Tetris(){
     initReader();
@@ -24,4 +19,36 @@ PlayerAction readLatestPlayerAction(){
     actionReady = false;
     struct Coordinate coord = coordFromADC(raw);
 	return actionFromCoordinate(coord);
+}
+
+void startDebounceTimer(){
+	TCCR3A=0b00000000; // Normal mode
+	TCCR3B=0b00000100; // Prescaler 256
+	TCNT3=((TIMER_MAX + 1)-((0.2 * CPU_CLOCK)/256)); // Count
+	TIMSK3=0b00000001;	
+}
+
+void stopDebouceTimer(){
+	TCCR3B = 0b00000000; // Stop timer
+	TIFR3 |= 0b00000001; // Set overflow flag
+}
+
+
+void reEnableTouch(){
+	EIFR |= 0b00010000; // Set interrupt 4 flag
+	EIMSK |= 0b00010000; // Enable interrupt 4
+}
+
+
+ISR(INT4_vect)
+{
+	EIMSK &= 0b11101111; // Disable interrupt 4
+	raw = getRawADCCoordates();
+	startDebounceTimer();
+    actionReady = true;
+}
+
+ISR(TIMER3_OVF_vect){
+	stopDebouceTimer();
+	reEnableTouch();
 }
